f5ccf52181bad744b7d36f1d88c6f6e379bd3a6a
enmanuel <enmanuelrondon54@gmail.com>
2023-09-22 16:45:16 -0400

    primer commit ing-enmanuel

---
+++ main.js
@@ -0,0 +1,11 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+  document.querySelector(".nav-menu").classList.toggle("show");
+});
+
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards'), {delay:500};
+ScrollReveal().reveal('.news-banner-one'), {delay:500};
+ScrollReveal().reveal('.news-banner-two'), {delay:500};
+
+
